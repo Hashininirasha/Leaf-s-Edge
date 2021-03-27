900ee1c68e84e7de6da303dc408af1c58ecf38b6
Hashininirasha <hashininirashamunasinghe@gmail.com>
2021-03-27 16:16:59 +0530

    import all html files

--- index.js
+++ index.js
@@ -122,5 +122,62 @@ $(document).ready(function(){
         $(this).addClass('active');
     });
 
+     //Gift owl carousel
+     $("#coming .owl-carousel").owlCarousel({
+        loop:true,
+        nav:true,
+        dots:false,
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:3
+            },
+            1000:{
+                items:5
+            }
+
+        }
+    });
+    //isotope filter
+    var $grid = $(".grid").isotope({
+      itemSelector:'.grid-item',
+      layoutMode:'fitRows'
+  });
+  //filter items on button click
+  $('.button-group').on("click","button",function(){
+      var filterValue = $(this).attr('data-filter');
+      $grid.isotope({filter:filterValue});
+  })
+   //Gift owl carousel
+   $("#gift .owl-carousel").owlCarousel({
+    loop:true,
+    nav:true,
+    dots:false,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:5
+        }
+
+    }
+});
+//isotope filter
+var $grid = $(".grid").isotope({
+  itemSelector:'.grid-item',
+  layoutMode:'fitRows'
+});
+//filter items on button click
+$('.button-group').on("click","button",function(){
+  var filterValue = $(this).attr('data-filter');
+  $grid.isotope({filter:filterValue});
+})
+
 
     });
