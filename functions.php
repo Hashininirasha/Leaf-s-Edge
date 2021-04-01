80c1fa73870226ae6c3fe875866bc4b9f6cdc74a
Hashininirasha <hashininirashamunasinghe@gmail.com>
2021-04-01 19:11:58 +0530

    fetch product data one by one

--- functions.php
+++ functions.php
@@ -3,7 +3,13 @@
 //require MySql Connection
 require ('database/DBController.php');
 
+//require product class
+require ('database/product.php');
+
  //DB controller object
  $db = new DBController();
+
+ //product object
+ $product = new Product($db);
 
  
