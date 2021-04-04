fff4bddf88f5058ac758e44129cba1565033cbcb
Hashininirasha <hashininirashamunasinghe@gmail.com>
2021-04-04 20:00:31 +0530

    added product header and update blogs

--- Template/_top-sale.php
+++ Template/_top-sale.php
@@ -10,12 +10,12 @@
                 <!--Owl Carousel-->
                 
                 <div class="owl-carousel owl-theme">
-                <?php foreach ($product_shuffle as $item) { ?>
+                
                     
                     <div class="item py-2">
 
                         <div class="product font-rale">
-                        <a href="<?php printf('%s?item_id=%s', 'product.php',  $item['item_id']); ?>"><img src="<?php echo $item['item_image'] ??"./assest/products/T1.jpg";?>" alt="product" class="img-fluid"></a><br>
+                        <a href="#"><img src="./assest/products/T1.jpg" alt="product" class="img-fluid"></a><br>
                             <div class="text-center">
                                 <h6>Aeonium with Dino</h6>
                             <div class="rating text-warning font-size-12">
@@ -28,7 +28,7 @@
                                 </span>
                             </div>
                             <div class="price py-2">
-                                <span>Rs.<? php echo $item['item_price'] ?? '0';?></span>
+                                <span>Rs.850></span>
                             </div>
                             <button type="submit" class="btn btn-warning font-size-12"><i class="fas fa-shopping-cart"></i>|Add to cart</button>
                                 </div>
@@ -36,8 +36,152 @@
                     
                    
                     </div>
+                    <div class="item py-2">
+                        <div class="product font-rale">
+                            <a href="#"><img src="./assest/products/T2.jpg" alt="product" class="img-fluid"></a><br>
+                            <div class="text-center">
+                                <h6>Fox Head Aeonium</h6>
+                            <div class="rating text-warning font-size-12">
+                                <span>
+                                    <i class="fas fa-star"></i>
+                                    <i class="fas fa-star"></i>
+                                    <i class="fas fa-star"></i>
+                                    <i class="fas fa-star"></i>
+                                    <i class="far fa-star"></i>
+                                </span>
+                            </div>
+                            <div class="price py-2">
+                                <span>Rs.780</span>
+                            </div>
+                            <button type="submit" class="btn btn-warning font-size-12"><i class="fas fa-shopping-cart"></i>|Add to cart</button>
+                                </div>
+                                
+
+                                
+                        </div>
+                    </div>
+                    <div class="item py-2">
+                        <div class="product font-rale">
+                            <a href="#"><img src="./assest/products/T3.jpg" alt="product" class="img-fluid"></a><br>
+                            <div class="text-center">
+                                <h6>Baw arboreum</h6>
+                            <div class="rating text-warning font-size-12">
+                                <span>
+                                    <i class="fas fa-star"></i>
+                                    <i class="fas fa-star"></i>
+                                    <i class="fas fa-star"></i>
+                                    <i class="fas fa-star"></i>
+                                    <i class="far fa-star"></i>
+                                </span>
+                            </div>
+                            <div class="price py-2">
+                                <span>Rs.670</span>
+                            </div>
+                            <button type="submit" class="btn btn-warning font-size-12"><i class="fas fa-shopping-cart"></i>|Add to cart</button>
+                                </div>
+                                
+
+                                
+                        </div>
+                    </div>
+                    <div class="item py-2">
+                        <div class="product font-rale">
+                            <a href="#"><img src="./assest/products/T4.jpg" alt="product" class="img-fluid"></a><br>
+                            <div class="text-center">
+                                <h6>Money plant Girl</h6>
+                            <div class="rating text-warning font-size-12">
+                                <span>
+                                    <i class="fas fa-star"></i>
+                                    <i class="fas fa-star"></i>
+                                    <i class="fas fa-star"></i>
+                                    <i class="fas fa-star"></i>
+                                    <i class="far fa-star"></i>
+                                </span>
+                            </div>
+                            <div class="price py-2">
+                                <span>Rs.1 200</span>
+                            </div>
+                            <button type="submit" class="btn btn-warning font-size-12"><i class="fas fa-shopping-cart"></i>|Add to cart</button>
+                                </div>
+                                
+
+                                
+                        </div>
+                    </div>
+                    <div class="item py-2">
+                        <div class="product font-rale">
+                            <a href="#"><img src="./assest/products/T5.jpg" alt="product" class="img-fluid"></a><br>
+                            <div class="text-center">
+                                <h6>Aeonium Cat collection</h6>
+                            <div class="rating text-warning font-size-12">
+                                <span>
+                                    <i class="fas fa-star"></i>
+                                    <i class="fas fa-star"></i>
+                                    <i class="fas fa-star"></i>
+                                    <i class="fas fa-star"></i>
+                                    <i class="far fa-star"></i>
+                                </span>
+                            </div>
+                            <div class="price py-2">
+                                <span>Rs.2 300</span>
+                            </div>
+                            <button type="submit" class="btn btn-warning font-size-12"><i class="fas fa-shopping-cart"></i>|Add to cart</button>
+                                </div>
+                                
+
+                                
+                        </div>
+                    </div>
+                    <div class="item py-2">
+                        <div class="product font-rale">
+                            <a href="#"><img src="./assest/products/T6.jpg" alt="product" class="img-fluid"></a><br>
+                            <div class="text-center">
+                                <h6>Kokedama Plant</h6>
+                            <div class="rating text-warning font-size-12">
+                                <span>
+                                    <i class="fas fa-star"></i>
+                                    <i class="fas fa-star"></i>
+                                    <i class="fas fa-star"></i>
+                                    <i class="fas fa-star"></i>
+                                    <i class="far fa-star"></i>
+                                </span>
+                            </div>
+                            <div class="price py-2">
+                                <span>Rs.1 400</span>
+                            </div>
+                            <button type="submit" class="btn btn-warning font-size-12"><i class="fas fa-shopping-cart"></i>|Add to cart</button>
+                                </div>
+                                
+
+                                
+                        </div>
+                    </div>
+                    <div class="item py-2">
+                        <div class="product font-rale">
+                            <a href="#"><img src="./assest/products/T7.jpg" alt="product" class="img-fluid"></a><br>
+                            <div class="text-center">
+                                <h6>Owl Hair</h6>
+                            <div class="rating text-warning font-size-12">
+                                <span>
+                                    <i class="fas fa-star"></i>
+                                    <i class="fas fa-star"></i>
+                                    <i class="fas fa-star"></i>
+                                    <i class="fas fa-star"></i>
+                                    <i class="far fa-star"></i>
+                                </span>
+                            </div>
+                            <div class="price py-2">
+                                <span>Rs.850</span>
+                            </div>
+                            <button type="submit" class="btn btn-warning font-size-12"><i class="fas fa-shopping-cart"></i>|Add to cart</button>
+                                </div>
+                                
+
+                                
+                        </div>
+                    </div>
                 
-                    <?php } // closing foreach function ?>
+                    
                 </div>
             <!--!Owl Carousel-->
             </div>
--- product.php
+++ product.php
@@ -1,6 +1,6 @@
 <?php
 //include header.php
-include('header.php');
+include('product_header.php');
 ?>
 
 
